d222b83aa1e45d0553dbaaa938bbc779bf92b57e
Angervuori Jouko <jangervu@t40561-ubu03.org.aalto.fi>
2021-02-09 18:02:46 +0200

    hei

---
+++ internals.pl
@@ -0,0 +1,22 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_69/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_70/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_shadow/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_stochasticgeometry/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
--- labels.pl
+++ labels.pl
@@ -2,6 +2,22 @@
 # Associate labels original text with physical files.
 
 
+$key = q/cite_69/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_70/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_shadow/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_stochasticgeometry/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
 1;
 
 
