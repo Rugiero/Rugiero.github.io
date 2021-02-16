b06ad04c7cdc9d3e3e29d066471e032fcabd7794
Angervuori Jouko <jangervu@t40561-ubu03.org.aalto.fi>
2021-02-16 13:04:46 +0200

    update

--- labels.pl
+++ labels.pl
@@ -2,32 +2,12 @@
 # Associate labels original text with physical files.
 
 
-$key = q/1/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_stochasticgeometry/;
-$external_labels{$key} = "$URL/" . q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:footprint/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:mesh1/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
 1;
 
 
 # LaTeX2HTML 2019.2 (Released June 5, 2019)
 # labels from external_latex_labels array.
 
-
-$key = q/1/;
-$external_latex_labels{$key} = q|4.1.1|; 
-$noresave{$key} = "$nosave";
 
 $key = q/_/;
 $external_latex_labels{$key} = q|<|; 
