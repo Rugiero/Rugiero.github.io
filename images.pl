2b5f8fbfbb6d77285225bc6ad3ad479dc503af08
Angervuori Jouko <jangervu@t40561-ubu03.org.aalto.fi>
2021-02-17 18:45:59 +0200

    Februarypost

--- images.pl
+++ images.pl
@@ -2,9 +2,26 @@
 # Associate images original text with physical files.
 
 
-$key = q/2020.;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG STYLE="height: 1.72ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
- ALT="$2020.$">|; 
+$key = q/(r,theta,varphi);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img4.svg"
+ ALT="$(r,\theta,\varphi)$">|; 
+
+$key = q/A;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.81ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$A$">|; 
+
+$key = q/A=[-pi,pi]times[-1,1];MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img1.svg"
+ ALT="$A = [-\pi,\pi] \times [-1,1]$">|; 
+
+$key = q/displaystyle(x,y)mapsto(r,x,sin(y))nonumber.;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img3.svg"
+ ALT="$\displaystyle (x,y) \mapsto (r,x,\sin(y)) \nonumber.
+$">|; 
+
+$key = q/r;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.23ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img5.svg"
+ ALT="$r$">|; 
 
 1;
 
