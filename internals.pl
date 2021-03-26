c45a5bad1d6bc3d36ece5811c83274a96c455cf7
Angervuori Jouko <jangervu@t40565-ubu07.org.aalto.fi>
2021-03-26 17:12:25 +0200

    March

--- internals.pl
+++ internals.pl
@@ -1,17 +1,21 @@
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate internals original text with physical files.
 
 
 $key = q/cite_2/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_3/;
 $ref_files{$key} = "$dir".q|node7.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_pass/;
-$ref_files{$key} = "$dir".q|node9.html|; 
+$ref_files{$key} = "$dir".q|node7.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_theoryofpoint/;
-$ref_files{$key} = "$dir".q|node7.html|; 
+$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- labels.pl
+++ labels.pl
@@ -1,23 +1,27 @@
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate labels original text with physical files.
 
 
 $key = q/cite_2/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_3/;
 $external_labels{$key} = "$URL/" . q|node7.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_pass/;
-$external_labels{$key} = "$URL/" . q|node9.html|; 
+$external_labels{$key} = "$URL/" . q|node7.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_theoryofpoint/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 1;
 
 
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # labels from external_latex_labels array.
 
 
