48bd386dcf01b648288ac34384e34b2be99e98a2
Angervuori Jouko <jangervu@t40565-ubu07.org.aalto.fi>
2021-03-26 17:25:40 +0200

    hu

--- internals.pl
+++ internals.pl
@@ -10,6 +10,18 @@ $key = q/cite_3/;
 $ref_files{$key} = "$dir".q|node7.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_Fran__ois_Baccelli__Bartlomiej_Blaszczyszyn___SPMldquo_Stochastic_Geometry_and_Wireless_Networks_SPMrdquo_/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hal1/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hurr/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_pass/;
 $ref_files{$key} = "$dir".q|node7.html|; 
 $noresave{$key} = "$nosave";
--- labels.pl
+++ labels.pl
@@ -10,6 +10,18 @@ $key = q/cite_3/;
 $external_labels{$key} = "$URL/" . q|node7.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_Fran__ois_Baccelli__Bartlomiej_Blaszczyszyn___SPMldquo_Stochastic_Geometry_and_Wireless_Networks_SPMrdquo_/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hal1/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hurr/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_pass/;
 $external_labels{$key} = "$URL/" . q|node7.html|; 
 $noresave{$key} = "$nosave";
