c38d591a23b3b050b5ef554223f8d2c030b9d67d
Angervuori Jouko <jangervu@t40565-ubu07.org.aalto.fi>
2021-05-12 17:04:21 +0300

    typos

--- internals.pl
+++ internals.pl
@@ -1,9 +1,21 @@
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate internals original text with physical files.
 
 
 $key = q/cite_2/;
 $ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_3/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Fran__ois_Baccelli__Bartlomiej_Blaszczyszyn___SPMldquo_Stochastic_Geometry_and_Wireless_Networks_SPMrdquo_/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hal1/;
+$ref_files{$key} = "$dir".q|node8.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_hurr/;
--- labels.pl
+++ labels.pl
@@ -1,9 +1,21 @@
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate labels original text with physical files.
 
 
 $key = q/cite_2/;
 $external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_3/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Fran__ois_Baccelli__Bartlomiej_Blaszczyszyn___SPMldquo_Stochastic_Geometry_and_Wireless_Networks_SPMrdquo_/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hal1/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_hurr/;
@@ -21,7 +33,7 @@ $noresave{$key} = "$nosave";
 1;
 
 
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # labels from external_latex_labels array.
 
 
