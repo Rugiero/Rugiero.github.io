c018e0c54ab341fdd5fc9a9d732f6dc3a08acad1
Angervuori Jouko <jangervu@t40561-ubu03.org.aalto.fi>
2022-05-12 13:40:18 +0300

    H

--- labels.pl
+++ labels.pl
@@ -1,33 +1,21 @@
-# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
 # Associate labels original text with physical files.
 
 
-$key = q/cite_2/;
-$external_labels{$key} = "$URL/" . q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hal2/;
+$key = q/singleantenna/;
 $external_labels{$key} = "$URL/" . q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hurr/;
-$external_labels{$key} = "$URL/" . q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_pass/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_theoryofpoint/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
 $noresave{$key} = "$nosave";
 
 1;
 
 
-# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
 # labels from external_latex_labels array.
 
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
 
 1;
 
