b9dddd9fd2636ee2f5c3fda2287e063717451308
Ilari Angervuori <iangervuori@gmail.com>
2022-06-27 18:12:37 +0300

    H

--- internals.pl
+++ internals.pl
@@ -1,25 +1,9 @@
-# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
 # Associate internals original text with physical files.
 
 
-$key = q/cite_2/;
-$ref_files{$key} = "$dir".q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hal2/;
+$key = q/singleantenna/;
 $ref_files{$key} = "$dir".q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hurr/;
-$ref_files{$key} = "$dir".q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_pass/;
-$ref_files{$key} = "$dir".q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_theoryofpoint/;
-$ref_files{$key} = "$dir".q|node10.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- labels.pl
+++ labels.pl
@@ -2,26 +2,6 @@
 # Associate labels original text with physical files.
 
 
-$key = q/cite_2/;
-$external_labels{$key} = "$URL/" . q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hal2/;
-$external_labels{$key} = "$URL/" . q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hurr/;
-$external_labels{$key} = "$URL/" . q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_pass/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_theoryofpoint/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
 1;
 
 
