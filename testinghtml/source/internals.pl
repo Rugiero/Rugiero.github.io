1a6132f3df2e3594c3ec86a617dc2f14ee1ca2cc
Ilari <iangervuori@gmail.com>
2026-01-13 19:17:04 +0200

    H

---
+++ testinghtml/source/internals.pl
@@ -0,0 +1,14 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate internals original text with physical files.
+
+
+$key = q/eq:pollog/;
+$ref_files{$key} = "$dir".q|node30.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/line:22/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
